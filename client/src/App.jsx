0845c5492ba412f43f147e900c3bcc508a640069
hikigaya007 <bijop007@gmail.com>
2023-11-08 00:22:10 +0530

    done installations

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="m-0 p-0 flex justify-center">
+      <h1 className="bg-red-800 my-3 text-center  font-extrabold text-white m-0">Hello world</h1>
+    </div>
+  )
+}
