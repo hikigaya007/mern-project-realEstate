d4acd2bfcdea2f67e94199df57673510762be510
hikigaya007 <bijop007@gmail.com>
2023-11-22 11:15:23 +0530

    created sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs' ;
+import { errorHandler } from "../utils/error.js";
+
+import  jwt from "jsonwebtoken";
 
 export const signup = async (req , res , next) => {
    const {username , email , password} = req.body;
@@ -16,3 +19,27 @@ export const signup = async (req , res , next) => {
    }
 
 };
+
+// sign in auth
+export const signin = async (req , res, next ) => {
+   const {username , password } = req.body;
+   try {
+      const validUser = await User.findOne({username});
+      if (!validUser) return next(errorHandler(404, 'User Not Found'));
+      const validPassword = bcryptjs.compareSync(password , validUser.password);
+      if (!validPassword) return next(errorHandler(401, "Invalid Password"));
+      const token = jwt.sign({id : validUser._id}, process.env.JWT_SECRET)
+
+      const {password: pass, ...rest} = validUser._doc ; 
+
+      res
+      .cookie('accessToken',token, {httpOnly: true })
+      .status(200)
+      .json(rest
+         );
+
+   } catch (error) {
+      next(error);
+      
+   }
+}
