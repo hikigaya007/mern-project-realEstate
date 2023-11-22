1cc08dce09a4d17d3fa4fd6c7d2ab7691bb07981
hikigaya007 <bijop007@gmail.com>
2023-11-22 10:42:49 +0530

    completed the signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
 
 function SignUp() {
 
   const [formData , setFormData] = useState({});
+  const [error , setError] = useState(null);
+  const [loading , setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
   const handleChange = (e) =>{
     setFormData({
       ...formData , 
@@ -13,6 +19,10 @@ function SignUp() {
 
   const handleSubmit = async (e) =>{
     e.preventDefault();
+    try {
+      
+    setLoading(true);
+
     const res = await fetch ('/api/auth/signup',
 
     {
@@ -24,10 +34,23 @@ function SignUp() {
     }
   );
     const data = await res.json();
-    console.log(data);
+
+    if (data.success === false){
+      setError(data.message);
+      setLoading(false)
+      return;
+    }
+    setLoading(false);
+    setError(null);
+
+    navigate('/signin');
+
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
 };
 
-  console.log(formData)
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -36,7 +59,8 @@ function SignUp() {
       <input type="text" placeholder='Username' className='border p-3 rounded-lg'  id='username' onChange={handleChange}/>
       <input type="email" placeholder='Email' className='border p-3 rounded-lg'  id='email' onChange={handleChange}/>
       <input type="password" placeholder='Password' className='border p-3 rounded-lg'  id='password' onChange={handleChange}/>
-      <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>sign up</button>
+      <button disabled = {loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+        {loading ? 'Loading...':'Sign Up'}</button>
     </form>
     <div className='flex gap-2 mt-5'>
       <p>Have an account ?</p>
@@ -44,6 +68,7 @@ function SignUp() {
         <span className='text-blue-600'>Sign In</span>
       </Link>
     </div>
+    {error && <p className='text-red-600 mt-2'>{error}</p>}
     </div>
   )
 }
