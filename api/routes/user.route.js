8a8b1bc740ec43e46ed69f1a593a7a26c5cf6791
hikigaya007 <bijop007@gmail.com>
2024-03-31 19:09:56 +0530

    Completed property page functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { test, updateUser,deleteUser , getUserListing } from "../controllers/user.controller.js";
+import { test, updateUser,deleteUser , getUserListing , getUser } from "../controllers/user.controller.js";
 import { verifyTokens } from "../utils/verifyUser.js";
 
 
@@ -10,5 +10,6 @@ router.get('/test',test);
 router.post('/update/:id',verifyTokens,updateUser)
 router.delete('/delete/:id',verifyTokens,deleteUser)
 router.get('/listings/:id',verifyTokens,getUserListing)
+router.get('/:id',verifyTokens, getUser)
 
 export default router;
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,12 +4,28 @@ import {Swiper , SwiperSlide} from 'swiper/react'
 import SwiperCore from 'swiper';
 import {Navigation} from 'swiper/modules'
 import 'swiper/css/bundle';
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from 'react-icons/fa';
+import {useSelector} from 'react-redux'
+import Contact from '../components/Contact';
 
 function Listing() {
 
   const params = useParams();
 
+  const {currentUser} = useSelector((state) => state.user)
+
   const [property , setProperty] = useState([]);
+
+  const [contact , setContact] = useState(false)
+
+  const [copied , setCopied] = useState(false);
 
   const [loading , setLoading] = useState(true);
 
@@ -63,7 +79,6 @@ function Listing() {
             return(
               <SwiperSlide key={url}>
                 <div className='h-[500px]' style={{background: `url(${url}) center no-repeat`}}>
-
                 </div>
               </SwiperSlide>
             )
@@ -72,6 +87,81 @@ function Listing() {
       </div>
 
       ) }
+
+      <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+            <FaShare
+              className='text-slate-500'
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+      </div>
+      {copied && (
+        <p className='fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2'>
+           Link copied!
+          </p>
+        )}
+        
+        <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+          <p className='text-2xl font-semibold'>
+                {property.name} - ${' '}
+                {property.offer
+                  ? property.discountedPrice
+                  : property.regularPrice}
+                {property.type === 'rent' && ' / month'}
+          </p>
+          <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
+              <FaMapMarkerAlt className='text-green-700' />
+              {property.address}
+          </p>
+
+          <div className='flex gap-4'>
+              <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                {property.type === 'rent' ? 'For Rent' : 'For Sale'}
+              </p>
+              {property.offer && (
+                <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                  ${+property.regularPrice - +property.discountedPrice} OFF
+                </p>
+              )}
+          </div>
+          <p className='text-slate-800'>
+              <span className='font-semibold text-black'>Description - </span>
+              {property.description}
+          </p>
+          <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <FaBed className='text-lg' />
+                {property.bedrooms > 1
+                  ? `${property.bedrooms} beds `
+                  : `${property.bedrooms} bed `}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <FaBath className='text-lg' />
+                {property.bathrooms > 1
+                  ? `${property.bathrooms} baths `
+                  : `${property.bathrooms} bath `}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <FaParking className='text-lg' />
+                {property.parking ? 'Parking spot' : 'No Parking'}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <FaChair className='text-lg' />
+                {property.furnished ? 'Furnished' : 'Unfurnished'}
+              </li>
+            </ul>
+            {currentUser && property.userRef !== currentUser._id && 
+            !contact && 
+            <button 
+            onClick={() => setContact(true)}
+            className='bg-slate-700 text-white p-3 rounded-lg'>Contact Landlord</button>}
+            {contact && <Contact listing={property}/>}
+        </div>
     </main>
   )
 }
