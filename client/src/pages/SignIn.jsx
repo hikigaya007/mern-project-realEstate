0659123db889d23b5b24cb847eb925e68e576171
hikigaya007 <bijop007@gmail.com>
2024-04-16 20:13:12 +0530

    Refactoring codes

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -55,7 +55,7 @@ function SignIn() {
 
 
   return (
-    <div className='p-3 max-w-lg mx-auto'>
+    <div className='p-3 max-w-lg mx-auto  '>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
     <form onSubmit={handleSubmit}  className='flex flex-col gap-4 '>
       <input type="text" placeholder='Username' className='border p-3 rounded-lg'  id='username' onChange={handleChange}/>
